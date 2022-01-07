a271486d39b4221517f3e4e27d93e074637f7bab
DominicusRA <dominicusrakaalfryann@gmail.com>
2022-01-07 19:41:05 +0700

    first deploy

--- application/controllers/Login_controler.php
+++ application/controllers/Login_controler.php
@@ -6,7 +6,7 @@ class Login_controler extends CI_Controller {
 		parent::__construct();
 		// session_start();
 		// $this->load->library('form_validation');
-		// $this->load->model('login_model');
+		$this->load->model('login_model');
     }
 	public function index()
 	{
