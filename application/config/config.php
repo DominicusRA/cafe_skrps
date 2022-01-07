df3359fb1a00ab5dc1297c18513e7b11c163a161
DominicusRA <dominicusrakaalfryann@gmail.com>
2022-01-07 19:26:39 +0700

    first deploy

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/cafe_skrps/';
+$config['base_url'] = 'https://cafe-skrps.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
