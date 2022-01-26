05f74509841580c2cd5e7e3737004c9031be419b
DominicusRA <dominicusrakaalfryann@gmail.com>
2022-01-27 00:04:10 +0700

    tampilan login

--- application/views/kasir/dashboard.php
+++ application/views/kasir/dashboard.php
@@ -31,10 +31,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
     <div class="container">
-      <a href="index3.html" class="navbar-brand">
-        <img src="<?php echo base_url() ?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">DREAM CAFE Coffe & Tea</span>
-      </a>
+      <img src="<?php echo base_url() ?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">DREAM CAFE Coffe & Tea</span>
 
       <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
--- application/views/login/register.php
+++ application/views/login/register.php
@@ -4,6 +4,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Dream Cafe || Register</title>
+  <style>
+   .bg-kopi {
+    background-image: url("<?php echo base_url() ?>assets/image/bg_kopi.jpeg");  
+   }
+   
+ </style>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -13,72 +19,79 @@
   <link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo base_url() ?>assets/dist/css/adminlte.min.css">
+  <!-- custom -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/dist/css/bg_kopi.css">
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <div class="login-logo">
-    <a href="#"><b>Dream Cafe</b> || coffe & tea</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Regist Your Account !!!</p>
-
-      <form action="<?php echo base_url() ?>index.php/login_controler/registeradd" method="post">
-        <div class="input-group mb-3">
-          <input type="text" name="username" class="form-control" placeholder="Username">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
-            </div>
-          </div>
+<body class="hold-transition login-page bg-kopi">
+  <div class="card bg-blur">
+    <div class="card-body">
+      <div class="login-box">
+        <div class="login-logo">
+          <p class="text-white"><b>Dream Cafe</b> || coffe & tea</p>
         </div>
-        <div class="input-group mb-3">
-          <input type="password" name="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <select name="akses" id="akses" class="form-control select2" style="width: 100%;">
-            <option selected="selected"></option>
-            <?php
-              foreach($akses->result_array() as $data_akses):
-            ?>
-            <option value="<?=$data_akses['id_akses']?>"><?=$data_akses['akses']?></option>
-            <?php
-              endforeach
-            ?>
-          </select>
-          <!-- <input type="password" name="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div> -->
-        </div>
-        <div class="row">
-          <!-- /.col -->
-          <div class="col-sm">
-            <a href="<?php echo base_url()?>index.php/login_controler/index">
-              <button type="button" class="btn btn-danger btn-block">Batal</button>
-            </a>
-          </div>
-          <div class="col-sm">
-            <button type="submit" class="btn btn-primary btn-block">Daftar</button>
-          </div>
-
-        </div>
-      </form>
-      <!-- /.social-auth-links -->
-
+        <!-- /.login-logo -->
+        <div class="card">
+          <div class="card-body login-card-body">
+            <p class="login-box-msg">Regist Your Account !!!</p>
       
+            <form action="<?php echo base_url() ?>index.php/login_controler/registeradd" method="post">
+              <div class="input-group mb-3">
+                <input type="text" name="username" class="form-control" placeholder="Username">
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-user"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="input-group mb-3">
+                <input type="password" name="password" class="form-control" placeholder="Password">
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-lock"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="input-group mb-3">
+                <select name="akses" id="akses" class="form-control select2" style="width: 100%;">
+                  <option selected="selected"></option>
+                  <?php
+                    foreach($akses->result_array() as $data_akses):
+                  ?>
+                  <option value="<?=$data_akses['id_akses']?>"><?=$data_akses['akses']?></option>
+                  <?php
+                    endforeach
+                  ?>
+                </select>
+                <!-- <input type="password" name="password" class="form-control" placeholder="Password">
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-lock"></span>
+                  </div>
+                </div> -->
+              </div>
+              <div class="row">
+                <!-- /.col -->
+                <div class="col-sm">
+                  <a href="<?php echo base_url()?>index.php/login_controler/index">
+                    <button type="button" class="btn btn-danger btn-block">Batal</button>
+                  </a>
+                </div>
+                <div class="col-sm">
+                  <button type="submit" class="btn btn-primary btn-block">Daftar</button>
+                </div>
+      
+              </div>
+            </form>
+            <!-- /.social-auth-links -->
+      
+            
+          </div>
+          <!-- /.login-card-body -->
+        </div>
+      </div>
+
     </div>
-    <!-- /.login-card-body -->
   </div>
-</div>
 <!-- /.login-box -->
 
 <!-- jQuery -->
