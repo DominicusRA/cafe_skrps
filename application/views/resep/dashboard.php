c5b02160fd7b32ebf5fcfb51b8a76d19701ca190
DominicusRA <dominicusrakaalfryann@gmail.com>
2022-02-23 23:03:28 +0700

    tambah modul navbar

--- application/views/resep/dashboard.php
+++ application/views/resep/dashboard.php
@@ -126,6 +126,12 @@
                   <p>Bahan</p>
                 </a>
               </li>
+              <li class="nav-item">
+                <a href="<?php echo base_url() ?>index.php/karyawan_controler" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Karyawan</p>
+                </a>
+              </li>
             </ul>
           </li>
 
