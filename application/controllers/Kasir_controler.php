cfc3fb84f0662e9a2f404ed84e8415a6343c466f
DominicusRA <dominicusrakaalfryann@gmail.com>
2022-06-18 12:46:22 +0700

    pembayaran

--- application/controllers/Kasir_controler.php
+++ application/controllers/Kasir_controler.php
@@ -1,75 +1,80 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Kasir_controler extends CI_Controller {
-	public function __construct(){
+class Kasir_controler extends CI_Controller
+{
+	public function __construct()
+	{
 		parent::__construct();
 		$this->load->model('kasir_model');
 		$this->load->model('login_model');
-    }
+	}
 	public function index()
 	{
 		// $this->load->view('kasir/dashboard');
 
-		if($this->login_model->cek_session()){
-			$data['cart']=null;
+		if ($this->login_model->cek_session()) {
+			$data['cart'] = null;
 
-            $data['menu']=$this->kasir_model->get_menu();
-			if($this->session->userdata('Cart')!=null){
-				$cart_data=array_count_values($this->session->userdata('Cart'));
-				$data['cart']=$this->kasir_model->get_cart(array_keys($cart_data));
-				$data['jumlah']=$cart_data;
+			$data['menu'] = $this->kasir_model->get_menu();
+			if ($this->session->userdata('Cart') != null) {
+				$cart_data = array_count_values($this->session->userdata('Cart'));
+				$data['cart'] = $this->kasir_model->get_cart(array_keys($cart_data));
+				$data['jumlah'] = $cart_data;
 			}
 
-            $this->load->view("kasir/dashboard",$data);
-
-        }else{
-            $this->load->view('login/login.php');
-        }
+			$this->load->view("kasir/dashboard", $data);
+		} else {
+			$this->load->view('login/login.php');
+		}
 	}
-	public function add_cart($menu){
+	public function add_cart($menu)
+	{
 		$cart_data = array();
-		if($this->session->userdata('Cart')!=null){
+		if ($this->session->userdata('Cart') != null) {
 			$cart_data = $this->session->userdata('Cart');
 		}
-		array_push($cart_data,$menu);
-		$this->session->set_userdata('Cart',$cart_data);
+		array_push($cart_data, $menu);
+		$this->session->set_userdata('Cart', $cart_data);
 		// if($this->session->set_userdata('Cart',$cart_data)){
 		// 	redirect('kasir_controler');
 		// }else{
-			// }
-			// $this->session->unset_userdata('Cart');
+		// }
+		// $this->session->unset_userdata('Cart');
 		redirect('kasir_controler');
 	}
-	public function delete_cart(){
+	public function delete_cart()
+	{
 		$this->session->unset_userdata('Cart');
 		redirect('kasir_controler');
 	}
-	public function bayar(){
+	public function bayar()
+	{
 		// echo $this->session->/
-		$tanggal_order= $this->input->post('tanggal_order');
+		$tanggal_order = $this->input->post('tanggal_order');
 		echo $tanggal_order;
-		$cart_data=[];
-		$data_nota=array(
+		echo "halo";
+		$cart_data = [];
+		$data_nota = array(
 			// 'tanggal' => date("Y/m/d"),
-			'tanggal' => $tanggal_order,
+			'tanggal' => date('Y/m/d'),
 			'no_nota' => '********'
 		);
-		
-		if($this->db->insert('nota',$data_nota)){
+
+		if ($this->db->insert('nota', $data_nota)) {
 			$id_nota = $this->db->insert_id();
 			$cart_data = $this->session->userdata('Cart');
 
 			for ($i = 0; $i < count($cart_data); $i++) {
-				$data_cart[$i]=array(
+				$data_cart[$i] = array(
 					'id_nota' => $id_nota,
 					'id_menu' => $cart_data[$i],
 					'jumlah' => 1
 				);
 			}
-			$status=$this->kasir_model->add_cart($data_cart);
+			$status = $this->kasir_model->add_cart($data_cart);
 		}
-		if($status){
+		if ($status) {
 			// print_r($cart_data);
 			// echo "<br>";
 			// print_r($data_cart);
@@ -79,60 +84,60 @@ class Kasir_controler extends CI_Controller {
 			redirect('kasir_controler');
 		}
 	}
-	public function nota_maker(){
+	public function nota_maker()
+	{
 		// $this->db->select('*');
 		// $this->db->from('nota');
-		
+
 
 		$this->db->select('*');
 		$this->db->from('nota');
 		$this->db->order_by('tanggal', 'DESC');
-		$this->db->limit(1); 
+		$this->db->limit(1);
 
-		$tanggal_nota=$this->db->get();
+		$tanggal_nota = $this->db->get();
 
 		// mulai sini
-		
-		foreach($tanggal_nota->result_array() as $data_tanggal_nota){
-			echo $tanggal=$data_tanggal_nota['tanggal'],"<br>";
-			
+
+		foreach ($tanggal_nota->result_array() as $data_tanggal_nota) {
+			echo $tanggal = $data_tanggal_nota['tanggal'], "<br>";
 		}
 		$datetime = new DateTime($tanggal);
-		$tanggl_loop=$datetime->format('Y-m-d');
-		
-		while($tanggl_loop<='2021-11-29'){
+		$tanggl_loop = $datetime->format('Y-m-d');
+
+		while ($tanggl_loop <= '2021-11-29') {
 			$datetime->modify('+1 day');
-			$tanggl_loop=$datetime->format('Y-m-d');
-			echo $tanggl_loop."/".$datetime->format('D'),"<br>";
-			if($datetime->format('D')!="Mon"){
-				$cart_data=array();
+			$tanggl_loop = $datetime->format('Y-m-d');
+			echo $tanggl_loop . "/" . $datetime->format('D'), "<br>";
+			if ($datetime->format('D') != "Mon") {
+				$cart_data = array();
 				// echo "LIBUR";
-				for($i=0;$i<mt_rand(1, 9);$i++){
+				for ($i = 0; $i < mt_rand(1, 9); $i++) {
 					// echo mt_rand(23, 36);
-					array_push($cart_data,mt_rand(23, 36));
+					array_push($cart_data, mt_rand(23, 36));
 				}
-				$data_nota=array(
+				$data_nota = array(
 					// 'tanggal' => date("Y/m/d"),
 					'tanggal' => $datetime->format('Y-m-d'),
 					'no_nota' => '********'
 				);
-				
-				if($this->db->insert('nota',$data_nota)){
+
+				if ($this->db->insert('nota', $data_nota)) {
 					$id_nota = $this->db->insert_id();
 					// $cart_data = $this->session->userdata('Cart');
 					// print_r($cart_data);
 					for ($i = 0; $i < count($cart_data); $i++) {
-						$data_cart[$i]=array(
+						$data_cart[$i] = array(
 							'id_nota' => $id_nota,
 							'id_menu' => $cart_data[$i],
 							'jumlah' => 1
 						);
 					}
-					$status=$this->kasir_model->add_cart($data_cart);
+					$status = $this->kasir_model->add_cart($data_cart);
 				}
 				// print_r($cart_data);
 				unset($cart_data);
-			}else{
+			} else {
 				echo "LIBUR";
 			}
 		}
--- application/views/kasir/dashboard.php
+++ application/views/kasir/dashboard.php
@@ -4,6 +4,7 @@ This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -27,218 +28,222 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- custom -->
   <link rel="stylesheet" href="<?php echo base_url() ?>assets/dist/css/bg_kopi.css">
 </head>
+
 <body class="hold-transition layout-top-nav">
-<div class="wrapper">
+  <div class="wrapper">
 
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
-    <div class="container">
-      <img src="<?php echo base_url() ?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">DREAM CAFE Coffe & Tea</span>
-
-      <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse order-3" id="navbarCollapse">
-        
-      </div>
-      
-      
-    </div>
-    <div class="navbar-custom-menu">
-      <ul class="nav navbar-nav">
-        <li class="notifications-menu">
-          <a href="<?php echo base_url() ?>index.php/login_controler/log_out">
-            <i class="fa fa-power-off"></i>
-            Log Out
-          </a>
-        </li>
-        
-        <li>
-          <!-- <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a> -->
-        </li>
-      </ul>
-      </div>
-  </nav>
-  <!-- /.navbar -->
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
       <div class="container">
-        <div class="row mb-2">
-          
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+        <img src="<?php echo base_url() ?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">DREAM CAFE Coffe & Tea</span>
 
-    <!-- Main content -->
-    <div class="content">
-      <div class="container overflow-auto">
-        <div class="row">
-          <div class="col-lg-8">
-            <div class="card card-row">
-              <div class="card-body overflow-auto">
+        <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-                <div class="container">
-                  <div class="row">
-                    <?php
-                      $nomor=0;
-                      
-                      foreach($menu->result_array() as $data_menu):
+        <div class="collapse navbar-collapse order-3" id="navbarCollapse">
+
+        </div>
+
+
+      </div>
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li class="notifications-menu">
+            <a href="<?php echo base_url() ?>index.php/login_controler/log_out">
+              <i class="fa fa-power-off"></i>
+              Log Out
+            </a>
+          </li>
+
+          <li>
+            <!-- <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a> -->
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container">
+          <div class="row mb-2">
+
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <div class="content">
+        <div class="container overflow-auto">
+          <div class="row">
+            <div class="col-lg-8">
+              <div class="card card-row">
+                <div class="card-body overflow-auto">
+
+                  <div class="container">
+                    <div class="row">
+                      <?php
+                      $nomor = 0;
+
+                      foreach ($menu->result_array() as $data_menu) :
                         $nomor++;
-                    ?>
-                    <div class="col-lg-4 " id="menu">
-                      <a href="<?php echo base_url() ?>index.php/kasir_controler/add_cart/<?=$data_menu['id_menu']?>" style="color: black">
-                        <div class="card">
-                          <!-- <img src="<?php echo base_url() ?>assets/dist/img/default-150x150.png" alt="" class="card-img-top"> -->
-                          <img src="<?php echo base_url() ?>assets/image/menu/<?=$data_menu['gambar']?>" alt="" class="card-img-top img-menu">
-                          
-                          <div class="card-body">
+                      ?>
+                        <div class="col-lg-4 " id="menu">
+                          <a href="<?php echo base_url() ?>index.php/kasir_controler/add_cart/<?= $data_menu['id_menu'] ?>" style="color: black">
+                            <div class="card">
+                              <!-- <img src="<?php echo base_url() ?>assets/dist/img/default-150x150.png" alt="" class="card-img-top"> -->
+                              <img src="<?php echo base_url() ?>assets/image/menu/<?= $data_menu['gambar'] ?>" alt="" class="card-img-top img-menu">
 
-                            <!-- <input type="text" id="menuu" name="id_menu" value="<?=$data_menu['id_menu']?>"> -->
-                            <div class="row">
-                              <div class="col">
-                                <?=$data_menu['nama']?>
+                              <div class="card-body">
 
+                                <!-- <input type="text" id="menuu" name="id_menu" value="<?= $data_menu['id_menu'] ?>"> -->
+                                <div class="row">
+                                  <div class="col">
+                                    <?= $data_menu['nama'] ?>
+
+                                  </div>
+                                  <div class="col float-right">
+                                    Rp<?= isset($data_menu['harga']) ? $data_menu['harga'] : '0' ?>
+
+                                  </div>
+
+                                </div>
                               </div>
-                              <div class="col float-right">
-                                Rp<?=isset($data_menu['harga']) ? $data_menu['harga'] : '0' ?>
-
-                              </div>
-                              
                             </div>
-                          </div>
+                          </a>
                         </div>
-                      </a>
+                      <?php
+                      endforeach
+                      ?>
                     </div>
-                    <?php
-                      endforeach  
-                    ?>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <!-- /.col-md-6 -->
-          
-          <div class="col-lg-4">
-            <div class="card h-100">
-              <div class="card-header">
-                <h5 class="card-title m-0">Nota</h5>
-                <div class="card-tools">
-                  <form action="<?php echo base_url() ?>index.php/kasir_controler/bayar" method="post">
-                  <!-- <input type="date" class="form-control" name="tanggal_order"> -->
-                  
-                  <a href="<?php echo base_url() ?>index.php/kasir_controler/delete_cart" >
-                    <button type="button" class="btn btn-outline-danger btn-sm" id="delete_cart">
-                      <i class="fa fa-trash"></i>
-                    </button>
-                  </a>
-                  <!-- <a href="<?php echo base_url() ?>index.php/kasir_controler/nota_maker" >
+            <!-- /.col-md-6 -->
+
+            <div class="col-lg-4">
+              <div class="card h-100">
+                <div class="card-header">
+                  <h5 class="card-title m-0">Nota</h5>
+                  <div class="card-tools">
+                    <!-- <form id="data_nota" action="<?php echo base_url() ?>index.php/kasir_controler/bayar" method="post">
+                      <input type="text" value="<?= date('d/m/Y') ?>" class="form-control" name="tanggal_order">
+                      <input type="text" class="form-control" id="iniTest" name="initest">
+                    </form> -->
+
+                    <a href="<?php echo base_url() ?>index.php/kasir_controler/delete_cart">
+                      <button type="button" class="btn btn-outline-danger btn-sm" id="delete_cart">
+                        <i class="fa fa-trash"></i>
+                      </button>
+                    </a>
+                    <!-- <a href="<?php echo base_url() ?>index.php/kasir_controler/nota_maker" >
                     <button type="button" class="btn btn-outline-danger btn-sm" id="delete_cart">
                       <i >MUEHEHEH</i>
                     </button>
                   </a> -->
+                  </div>
                 </div>
-              </div>
-              <div class="card-body">
-                <div class="container" id="nota">
-                  <?php
-                    $total_harga=0;
-                    if($cart!=null){
+                <div class="card-body">
+                  <div class="container" id="nota">
+                    <?php
+                    $total_harga = 0;
+                    if ($cart != null) {
                       // print_r($jumlah);
-                      foreach($cart->result_array() as $data_cart):
-                        $total_harga+=$data_cart['harga']*$jumlah[$data_cart['id_menu']]
-                  ?>
-                  <a href="" style="color: black">
-                    <div class="row">
-                      <div class="col-sm">
-                        <div class="card">
-                          <div class="card-body">
-                            <div class="float-left">
-                              <?=$data_cart['nama']?>
-                            </div>
-                            <div class="float-right">
-                              <?=$jumlah[$data_cart['id_menu']]?> x 
-                              Rp <?=isset($data_cart['harga']) ? $data_cart['harga'] : '0' ?>
+                      foreach ($cart->result_array() as $data_cart) :
+                        $total_harga += $data_cart['harga'] * $jumlah[$data_cart['id_menu']]
+                    ?>
+                        <a href="" style="color: black">
+                          <div class="row">
+                            <div class="col-sm">
+                              <div class="card">
+                                <div class="card-body">
+                                  <div class="float-left">
+                                    <?= $data_cart['nama'] ?>
+                                  </div>
+                                  <div class="float-right">
+                                    <?= $jumlah[$data_cart['id_menu']] ?> x
+                                    Rp <?= isset($data_cart['harga']) ? $data_cart['harga'] : '0' ?>
+                                  </div>
+                                </div>
+                              </div>
                             </div>
                           </div>
+                        </a>
+                    <?php
+                      endforeach;
+                    }
+                    ?>
+                  </div>
+
+                </div>
+                <div class="card-footer">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col">
+                        <div class="float-left">
+                          TOTAL BAYAR
+                        </div>
+                        <div class="float-right">
+                          <b>Rp <?= $total_harga ?></b>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <a href="<?php echo base_url() ?>index.php/kasir_controler/bayar">
+                    <div class="container">
+                      <div class="row">
+                        <div class="col">
+                          <button type="submit" from="data_nota" class="btn btn-success btn-block">
+                            Bayar
+                          </button>
+
                         </div>
                       </div>
                     </div>
                   </a>
-                  <?php
-                      endforeach;
-                    }
-                  ?>
                 </div>
-
               </div>
-              <div class="card-footer">
-                <div class="container">
-                  <div class="row">
-                    <div class="col">
-                      <div class="float-left">
-                        TOTAL BAYAR 
-                      </div>
-                      <div class="float-right">
-                        <b>Rp <?=$total_harga?></b>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="container">
-                  <div class="row">
-                    <div class="col">
-                      <button type="button" class="btn btn-success btn-block" id="delete_cart">
-                        <form>
-                          Bayar
-                      </button>
 
-                    </div>
-                  </div>
-                </div>
-                <!-- </a> -->
-              </div>
             </div>
-
+            <!-- /.col-md-6 -->
           </div>
-          <!-- /.col-md-6 -->
-        </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-      
+          <!-- /.row -->
+        </div><!-- /.container-fluid -->
+
+      </div>
+      <!-- /.content -->
     </div>
-    <!-- /.content -->
+    <!-- /.content-wrapper -->
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+
+    <!-- Main Footer -->
+
   </div>
-  <!-- /.content-wrapper -->
+  <!-- ./wrapper -->
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
+  <!-- REQUIRED SCRIPTS -->
 
-  <!-- Main Footer -->
-  
-</div>
-<!-- ./wrapper -->
+  <!-- jQuery -->
+  <script src="<?php echo base_url() ?>assets/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="<?php echo base_url() ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="<?php echo base_url() ?>assets/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="<?php echo base_url() ?>assets/dist/js/demo.js"></script>
 
-<!-- REQUIRED SCRIPTS -->
-
-<!-- jQuery -->
-<script src="<?php echo base_url() ?>assets/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="<?php echo base_url() ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="<?php echo base_url() ?>assets/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="<?php echo base_url() ?>assets/dist/js/demo.js"></script>
-
-<script src="<?php echo base_url() ?>assets/dist/js/kasir/kasir.js"></script>
+  <script src="<?php echo base_url() ?>assets/dist/js/kasir/kasir.js"></script>
 </body>
+
 </html>
