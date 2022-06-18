f2d48f89c5d485240320efdf45333ea535b32a2e
DominicusRA <dominicusrakaalfryann@gmail.com>
2022-06-18 10:57:03 +0700

    login controler

--- application/controllers/Login_controler.php
+++ application/controllers/Login_controler.php
@@ -1,69 +1,74 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login_controler extends CI_Controller {
-	public function __construct(){
+class Login_controler extends CI_Controller
+{
+	public function __construct()
+	{
 		parent::__construct();
 		// session_start();
 		// $this->load->library('form_validation');
 		$this->load->model('login_model');
-    }
+	}
 	public function index()
 	{
 		$status = $this->login_model->cek_session();
-		if($status){
-            redirect("dashboard_controler");
-        }else{
-            $this->load->view('login/login');
-        }
+		if ($status) {
+			redirect("dashboard_controler");
+		} else {
+			$this->load->view('login/login');
+		}
 	}
 	public function cek_log()
 	{
 		// $username = $this->input->post("username", TRUE);
-        // $password = $this->input->post("password", TRUE);
+		// $password = $this->input->post("password", TRUE);
 		// $password = md5($password);
 		// $username = md5($username);
 		$password = md5($this->input->post("password", TRUE));
 		$username = md5($this->input->post("username", TRUE));
 
 		// $status = $this->login_model->cek_login(array('username'=>$username),array('password'=>$password));
-		$status = $this->login_model->cek_login_v2(array('username'=>$username),array('password'=>$password));
-		if($status!=FALSE){
-            foreach($status as $apps){
-                $session_data = array(
-                    'nama'=>$apps->nama,
-					'akses'=>$apps->akses
+		$status = $this->login_model->cek_login_v2(array('username' => $username), array('password' => $password));
+		if ($status != FALSE) {
+			foreach ($status as $apps) {
+				$session_data = array(
+					'nama' => $apps->nama,
+					'akses' => $apps->akses
 				);
-                $this->session->set_userdata($session_data);
-            }
+				$this->session->set_userdata($session_data);
+			}
 			echo $this->session->userdata('akses');
-			if($this->session->userdata('akses')=="admin"){
+			if ($this->session->userdata('akses') == "admin") {
 				redirect("dashboard_controler");
-			}else if($this->session->userdata('akses')=="kasir"){
+			} else if ($this->session->userdata('akses') == "kasir") {
 				redirect("kasir_controler");
 			}
-        }else{
-            $this->index();
-        }
+		} else {
+			$this->index();
+		}
 	}
-	public function log_out(){
-        $this->session->sess_destroy();
-        redirect("login_controler");
+	public function log_out()
+	{
+		$this->session->sess_destroy();
+		redirect("login_controler");
 	}
-	public function register(){
-		$data['akses']=$this->login_model->get_akses();
-		$this->load->view('login/register',$data);
+	public function register()
+	{
+		$data['akses'] = $this->login_model->get_akses();
+		$this->load->view('login/register', $data);
 	}
-	public function registeradd(){
+	public function registeradd()
+	{
 		// $username = $this->input->post("username", TRUE);
-        // $password = $this->input->post("password", TRUE);
+		// $password = $this->input->post("password", TRUE);
 		$password = md5($this->input->post("password", TRUE));
 		$username = md5($this->input->post("username", TRUE));
 		$akses = $this->input->post("akses", TRUE);
 
 		// $this->login_model->add($username,$password);
 
-		$this->login_model->add_v2($username,$password,$akses);
+		$this->login_model->add_v2($username, $password, $akses);
 		redirect('login_controler');
 	}
 }
