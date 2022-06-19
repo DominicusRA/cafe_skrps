069f67cab878af9897c1c2228f3a9db8e2113cfa
DominicusRA <dominicusrakaalfryann@gmail.com>
2022-06-19 15:52:17 +0700

    normalisasi

--- application/controllers/Login_controler.php
+++ application/controllers/Login_controler.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 // ob_start();
 
 class Login_controler extends CI_Controller
