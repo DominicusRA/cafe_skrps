9be8da77d52598f91202680c407aaa094d300908
DominicusRA <dominicusrakaalfryann@gmail.com>
2022-06-20 16:55:45 +0700

    test deploy

--- application/controllers/Login_controler.php
+++ application/controllers/Login_controler.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Login_controler extends CI_Controller
 {
-	public function __construct()
+	function __construct()
 	{
 		parent::__construct();
 		$this->load->model('login_model');
